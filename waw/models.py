a69bfaa5dad19aae29f3936bbe0b34fadf2d7bb1
ck1044 <kyum1044@gmail.com>
2020-11-21 18:08:29 +0900

    '?'

--- waw/models.py
+++ waw/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    hash_tag = models.CharField(max_length=50)
+    hash_tag2 = models.CharField(max_length=50)
+    content = models.TextField()
+    photo = models.ImageField(blank=True)
+    pin = models.DecimalField(max_digits=99999999, decimal_places=0)
+
+    def __str__(self):
+        return self.hash_tag
+
+
+def get_rendom():
+    return Post.objects.order_by("?").first()
+
