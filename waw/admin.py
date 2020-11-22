ec2fc935fdb3e818e9282ef496ad280d91a1110a
ck1044 <kyum1044@gmail.com>
2020-11-23 01:36:04 +0900

    intro_view 완성

--- waw/admin.py
+++ waw/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['hash_tag', 'content', 'pin']
+    # #hash tag, @pin, content 별로 검색기능 넣기
+    search_fields = ['hash_tag', 'content', 'pin']
+
+
+admin.site.register(Post, PostAdmin)
--- waw/views.py
+++ waw/views.py
@@ -1,6 +1,21 @@
-from django.http import HttpResponse
+from .models import Post
+from django.shortcuts import render, get_object_or_404
+
 
 def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    """
+    post 리스트 출력
+    """
+    post_list = Post.objects.order_by("?")
+    context = {'post_list': post_list}
+    return render(request, 'waw/post_list.html', context)
+    # C:/projects/waw/templates/waw/post_list.html
 
-#hellol#
+
+def detail(request, post_id):
+    """
+    post 내용 출력
+    """
+    post = get_object_or_404(Post, pk=post_id)
+    context = {'post': post}
+    return render(request, 'waw/post_detail.html', context)
