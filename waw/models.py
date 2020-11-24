d1f9ca6812ac9e890810e810300c8e7dfd3ba9a6
ck1044 <kyum1044@gmail.com>
2020-11-25 00:47:50 +0900

    'static_css_applied'

--- waw/models.py
+++ waw/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
 
-<<<<<<< HEAD
-
-=======
 class Post(models.Model):
     hash_tag = models.CharField(max_length=50)
     content = models.TextField()
@@ -12,5 +9,5 @@ class Post(models.Model):
 
     def __str__(self):
         return self.hash_tag
->>>>>>> dce61b5fd3bbd03db21dfcfa6d22f1e080bde1ce
+
 
